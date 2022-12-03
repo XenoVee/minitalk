37818b5b010f5f7072491aa3d93d99e7a663867a
rmaes <ikbenruudmaes@gmail.com>
2022-12-04 00:23:01 +0100

    bitchass mf refusing to work

--- client_dir/client.c
+++ client_dir/client.c
@@ -6,21 +6,59 @@
 /*   By: rmaes <rmaes@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/03 18:45:41 by rmaes         #+#    #+#                 */
-/*   Updated: 2022/12/03 20:10:34 by rmaes         ########   odam.nl         */
+/*   Updated: 2022/12/04 00:09:41 by rmaes         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 #include <signal.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-void	send(int m, char *arg)
+void	send(char *arg, char *pidstr)
 {
 	static int	pid = 0;
+	static char	*binary = NULL;
+	static int	i = 0;
 
 	if (pid == 0)
-		pid = ft_atoi(arg);
-	kill(pid, m);
+		pid = ft_atoi(pidstr);
+	if (binary == NULL)
+		binary = arg;
+	ft_printf("sending %c to %i", binary[i], pid);
+	if (binary[i] == '1')
+		kill(pid, SIGUSR2);
+	if (binary[1] == '0')
+		kill(pid, SIGUSR1);
+	i++;
+}
+
+char	*binary_conv(char *str)
+{
+	char	*ret;
+	int		i;
+	int		j[2];
+
+	ret = ft_calloc(sizeof(char), (ft_strlen(str) + 1) * 8);
+	i = 0;
+	j[0] = 0;
+	j[1] = 0;
+	while (str[i])
+	{
+		while (j[0] < 8)
+		{
+			if ((str[i] & 0x80) == 128)
+				ret[j[1] + j[0]] = '1';
+			else
+				ret[j[1] + j[0]] = '0';
+			str[i] = str[i] << 1;
+			j[0]++;
+		}
+		j[1] += j[0];
+		j[0] = 0;
+		i++;
+	}
+	return (ret);
 }
 
 void	handler(int sig, siginfo_t *siginfo, void *p)
@@ -28,7 +66,7 @@ void	handler(int sig, siginfo_t *siginfo, void *p)
 	siginfo = NULL;
 	p = NULL;
 	if (sig == SIGUSR1)
-		send(30, NULL);
+		send(0, NULL);
 	if (sig == SIGUSR2)
 		exit(0);
 }
@@ -36,6 +74,7 @@ void	handler(int sig, siginfo_t *siginfo, void *p)
 int	main(int argc, char **argv)
 {
 	struct sigaction	sa;
+	char				*binary;
 
 	sa.sa_handler = SIG_DFL;
 	sa.sa_sigaction = &handler;
@@ -43,11 +82,17 @@ int	main(int argc, char **argv)
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
+	ft_printf("pid: %i\n", getpid());
 	if (argc != 3)
 	{
-		ft_printf("argument err");
-		return (1);
+		ft_printf("Provide PiD and string");
+		exit(1);
 	}
-	send(31, argv[1]);
-	send(30, NULL);
+	binary = binary_conv(argv[2]);
+	ft_printf("%s\n", binary);
+	send(binary, argv[1]);
+	while (1)
+	{
+		pause();
+	}
 }
--- server_dir/server.c
+++ server_dir/server.c
@@ -6,7 +6,7 @@
 /*   By: rmaes <rmaes@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/03 18:23:28 by rmaes         #+#    #+#                 */
-/*   Updated: 2022/12/03 20:17:39 by rmaes         ########   odam.nl         */
+/*   Updated: 2022/12/04 00:20:34 by rmaes         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,32 @@
 void	handler(int sig, siginfo_t *siginfo, void *p)
 {
 	static int	pid = 0;
+	static char	c = 0;
+	static int	i = 0;
 
-	if (pid == 0)
+	ft_printf("here");
+	if (pid == 0 || (siginfo->si_pid != 0 && siginfo->si_pid != pid))
 		pid = siginfo->si_pid;
 	p = NULL;
-	ft_printf("signal got: %i\n", sig);
-	if (sig == SIGUSR1)
-		kill(pid, SIGUSR1);
 	if (sig == SIGUSR2)
-		kill(pid, SIGUSR2);
+		c += 0x80;
+	c = c >> 1;
+	i++;
+	if (i >= 8)
+	{
+		ft_printf("%c", c);
+		if (c == '\0')
+			kill(pid, SIGUSR2);
+		i = 0;
+	}
+	kill(pid, SIGUSR1);
+}
+
+void	no(int sig, siginfo_t *siginfo, void *p)
+{
+	siginfo = NULL;
+	p = NULL;
+	ft_printf("fuck your %i", sig);
 }
 
 int	main(void)
@@ -37,6 +54,7 @@ int	main(void)
 	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
 	ft_printf("%i\n", getpid());
+	sigaction(SIGINT, &sa, NULL);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
